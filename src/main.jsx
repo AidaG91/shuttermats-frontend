5689d44c61f9b7b09c7ce9607fb2321f29c1350d
AidaG91 <aida.garmus@gmail.com>
2026-07-22 09:12:53 +0200

    styles: basic and reset styles added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./styles/main.scss";
+import { RouterProvider } from "react-router";
+import { router } from "./config/router.jsx";
+import { ReadingProvider } from "./context/ReadingContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <ReadingProvider>
+    <RouterProvider router={router} />
+  </ReadingProvider>,
+);
